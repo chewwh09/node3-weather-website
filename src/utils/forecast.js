ded7011e0bae1c44f3a9d08a73c240dc5ce253b9
Chew Wei Hao <weishawtyhao9@gmail.com>
2021-05-19 00:10:40 +0800

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longtitude, latitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + 
-                ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+                ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
